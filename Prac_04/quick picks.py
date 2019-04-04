8ba8fb3b9d118e43e4425346b1daa1197e56420a
DaRealTaylor <taylor.fontes@my.jcu.edu.au>
2019-04-04 13:29:21 +1000

    change "v" to "j"

--- Prac_04/quick picks.py
+++ Prac_04/quick picks.py
@@ -12,7 +12,7 @@ def main():
         number_of_quick_picks = int(input("How many quick picks? "))
     for i in range(number_of_quick_picks):
         quick_pick = []
-        for v in range(NUMBERS_PER_LINE):
+        for j in range(NUMBERS_PER_LINE):
             number = random.randint(MINIMUM, MAXIMUM)
             while number in quick_pick:
                 number = random.randint(MINIMUM, MAXIMUM)
